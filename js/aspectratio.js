0226c0eb4d0facb871fbf6f4807231302769f87b
Maurice van Creij <maurice.vancreij@webqem.com>
2014-02-20 08:23:43 +1100

    Added more comprehensive build script

--- js/aspectratio.js
+++
@@ -1,7 +0,0 @@
-/*
-	Combined JavaScripts
-
-	@codekit-append "../src/useful.aspectratio.js"
-	@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
-	@codekit-append "../../useful-instances/src/useful.instances.js"
-*/
